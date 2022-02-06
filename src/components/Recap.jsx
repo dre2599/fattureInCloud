855fe5beb95ccfdd682195c7d7bb011300a90ecf
dre2599 <casconeandrea99@gmail.com>
2022-02-06 19:05:40 +0100

    update font-size

--- src/components/Recap.jsx
+++ src/components/Recap.jsx
@@ -41,6 +41,7 @@ const Recap = ({ data }) => {
     })
     
     const [selectedRecap, setSelectedRecap] = useState([])
+    const [show, setShow] = useState(false)
 
     // Elimina i duplicati
     let uniqueArray = selectedRecap.filter( (ele, ind) => ind === selectedRecap.findIndex( elem => elem.id === ele.id  ))
@@ -68,6 +69,7 @@ const Recap = ({ data }) => {
                         e.removed.forEach(el => {
                             el.classList.remove("selected");
                             setSelectedRecap([])
+                            setShow(false)
                         });
                     }}
 
@@ -75,19 +77,22 @@ const Recap = ({ data }) => {
                         e.afterAdded.forEach(el => {
                             el.classList.add("selected");
                             setSelectedRecap((current) => [...current, finalArray[el.id]])
+                            setShow(true)
                         });
                         e.afterRemoved.forEach(el => {
                             el.classList.remove("selected");
                             setSelectedRecap([])
+                            setShow(false)
                         });
                     }}
                 />
 
             </div>
             <div className="client-msg">
-                <p>Hai selezionato:</p>
-                {uniqueArray.map((item, i) => <p className='msg' key={i}>{item.mese}: {item.documenti>1  ? 'vi sono' : 'solo'}  {item.documenti} {item.documenti>1 ? 'documenti' : 'documento' } con un importo totale di {item.importo}$</p>)}
-                {console.log(uniqueArray)}
+                {show && <p>Hai selezionato:</p>}
+                {uniqueArray.map((item, i) =>
+                <p className='msg' 
+                    key={i}>{item.mese}: {item.documenti>1  ? 'vi sono' : 'solo'} {item.documenti} {item.documenti>1 ? 'documenti' : 'documento' } con un importo totale di {item.importo}$</p>)}
             </div>
         </>
     );
